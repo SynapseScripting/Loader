cf1dc355a4d5fbb4c2c2ef04228b5169b546d882
SynapseScripting <axaxax2332@gmail.com>
2025-09-07 18:07:49 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ local Place_ID_With_Matching_Luarmor_ID = {
     [16472538603] = "https://api.luarmor.net/files/v3/loaders/da4990bfada69d4d045dd063f5ee1f1e.lua"; -- Tha Bronx 3
     [18642421777] = "https://api.luarmor.net/files/v3/loaders/da4990bfada69d4d045dd063f5ee1f1e.lua"; -- Tha Bronx 3
     [13643807539] = "https://api.luarmor.net/files/v3/loaders/25f8b18221b729055f0c81199ba2e5ac.lua"; -- South Bronx
-    [93612682780562] = "https://api.luarmor.net/files/v3/loaders/25f8b18221b729055f0c81199ba2e5ac.lua"; -- South Bronx
+    [14413475235] = "https://api.luarmor.net/files/v3/loaders/25f8b18221b729055f0c81199ba2e5ac.lua"; -- South Bronx
 };
 
 if not Place_ID_With_Matching_Luarmor_ID[game.PlaceId] then
@@ -16,5 +16,6 @@ end
 script_key = getfenv().script_key
 
 loadstring(game:HttpGet(Place_ID_With_Matching_Luarmor_ID[game.PlaceId]))()
+
 
 
